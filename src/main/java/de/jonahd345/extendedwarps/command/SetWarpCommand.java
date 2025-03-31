c370328b58b9993ddc10c3043e911d8ee5bfa8d3
jonahd345 <info.minejona@gmail.com>
2025-03-31 08:56:22 +0200

    feat: implement empty TabCompleter for SetWarpCommand

--- src/main/java/de/jonahd345/extendedwarps/command/SetWarpCommand.java
+++ src/main/java/de/jonahd345/extendedwarps/command/SetWarpCommand.java
@@ -6,9 +6,12 @@ import de.jonahd345.extendedwarps.model.Warp;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
-public class SetWarpCommand implements CommandExecutor {
+import java.util.List;
+
+public class SetWarpCommand implements CommandExecutor, TabCompleter {
     private ExtendedWarps plugin;
 
     public SetWarpCommand(ExtendedWarps plugin) {
@@ -37,5 +40,10 @@ public class SetWarpCommand implements CommandExecutor {
             player.sendMessage(Config.getMessageWithPrefix(Config.MSG_SET_WARP_COMMAND_USAGE));
         }
         return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return List.of();
     }
 }
