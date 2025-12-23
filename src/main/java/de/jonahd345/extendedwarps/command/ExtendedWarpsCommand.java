75e920ddc6f5845e560531f2b6b0258b15026587
jonahd345 <contact@jonahd345.de>
2025-12-23 21:09:44 +0100

    feat: add tab completion for ExtendedWarpsCommand and enhance permission checks

--- src/main/java/de/jonahd345/extendedwarps/command/ExtendedWarpsCommand.java
+++ src/main/java/de/jonahd345/extendedwarps/command/ExtendedWarpsCommand.java
@@ -1,11 +1,16 @@
 package de.jonahd345.extendedwarps.command;
 
 import de.jonahd345.extendedwarps.ExtendedWarps;
+import de.jonahd345.extendedwarps.model.Warp;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 
-public class ExtendedWarpsCommand implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExtendedWarpsCommand implements CommandExecutor, TabCompleter {
     private ExtendedWarps plugin;
 
     public ExtendedWarpsCommand(ExtendedWarps plugin) {
@@ -14,7 +19,7 @@ public class ExtendedWarpsCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-        if (!sender.hasPermission("extendedwarps.command.extendedwarps")) {
+        if (!sender.hasPermission("extendedwarps.command.extendedwarps") || !sender.hasPermission("extendedwarps.admin")) {
             sender.sendMessage(plugin.getMessageSettings().noPermission());
             return true;
         }
@@ -35,5 +40,27 @@ public class ExtendedWarpsCommand implements CommandExecutor {
             sender.sendMessage(plugin.getMessageSettings().delWarpCommandUsage());
         }
         return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        ArrayList<String> subcommand = new ArrayList<>();
+
+        if (sender.hasPermission("extendedwarps.command.extendedwarps") || sender.hasPermission("extendedwarps.admin")) {
+            if (args.length == 1) {
+                subcommand.add("reload");
+                subcommand.add("help");
+            }
+        }
+        ArrayList<String> cl = new ArrayList<>();
+        String currentarg = args[args.length - 1].toLowerCase();
+
+        for(String s1 : subcommand) {
+            String s2 = s1.toLowerCase();
+            if(s2.startsWith(currentarg)) {
+                cl.add(s1);
+            }
+        }
+        return cl;
     }
 }
